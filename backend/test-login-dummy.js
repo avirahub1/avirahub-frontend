2dc362b21edc2bd9037f8d1604ea6abf1cacc68e
Avira hub <rarrs5152@gmail.com>
2025-12-18 19:49:59 +0530

    Delete backend directory

--- backend/test-login-dummy.js
+++
@@ -1,8 +0,0 @@
-
-const fetch = require('node-fetch'); // backend has it or not? Node 18+ has global fetch.
-// Avira backend type is module, so we can use import or global fetch if node is recent.
-// package.json says type: module.
-
-// Let's use a curl command instead of a js file to avoid issues.
-// But wait, run_command for curl.
-console.log("Use curl");
